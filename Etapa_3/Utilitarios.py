a9150a2bb55b3cb5b0b20e3958e65e51fcf0a9c7
GiovannaSLisboa <110005344+GiovannaSLisboa@users.noreply.github.com>
2025-06-28 13:28:58 -0300

    Versão final

--- Etapa_3/Utilitarios.py
+++ Etapa_3/Utilitarios.py
@@ -6,21 +6,25 @@ def imprimir_dicionario(dados):
 def reconstruir_caminho(pred, origem, destino):
     caminho = []
     cur = destino
+    
+    if origem == destino:
+        return [origem] 
+    
+    if origem < 0 or origem >= len(pred) or \
+       destino < 0 or destino >= len(pred[0]) or \
+       pred[origem][destino] == -1:
+        return []
+    
     while cur != origem:
         caminho.append(cur)
+        
+        if pred[origem][cur] == -1:
+            return [] 
         cur = pred[origem][cur]
-        if cur == -1:
+        
+        if cur == -1: 
             return []
+            
     caminho.append(origem)
     caminho.reverse()
     return caminho
-
-
-def ler_capacidade(nome_arquivo):
-    with open(nome_arquivo, 'r') as f:
-        for linha in f:
-            if linha.strip().startswith('Capacity:'):
-                partes = linha.strip().split()
-                if len(partes) >= 2:
-                    return int(partes[1])
-    return None
